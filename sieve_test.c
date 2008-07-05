b34ef4221e95d80a2802990c0a1a917852e9ff59
mike <mike@panzerschreck.(none)>
2008-07-05 07:39:41 -0400

    Farmed out printing the results in "sieve_test.c" to a separate function, as it should have been to begin with (much easier bookkeeping a la gprof(1) ).

--- sieve_test.c
+++ sieve_test.c
@@ -3,19 +3,20 @@
 #include <stdlib.h>
 #include "sieve.h"
 
+void print_sieve(sieve_t s){
+		for(size_t i=0; i<max+1; ++i){
+				if(s[i])
+						printf("%u\n", i);
+		}
+}
+
 int main(int argc, char **argv){
 		assert(2 == argc);
 
 		size_t max = strtoul(argv[1], NULL, 10);
 		sieve_t my_sieve = sieve(max);
-		
-		for(size_t i=0; i<max+1; ++i){
-				if(my_sieve[i])
-						printf("%u\n", i);
-		}
-
+		print_sieve(my_sieve);
 		free(my_sieve);
 
 		return 0;
 }
-
